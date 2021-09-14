1a6ef0eb9766fae85ee5aeb61f9a65da790f2074
ortizcam <90529335+ortizcam@users.noreply.github.com>
2021-09-14 16:30:04 -0400

    Add files via upload

---
+++ Week 2/Assignments/Exercise2.0.py
@@ -0,0 +1,9 @@
+# This is for the class CIS104, exercise 2.0
+# Giving values to name, date, question, and answer
+name = ('Cameron Ortiz')
+date = ('September 14th, 2021')
+question = ('At what size of a project should you start using version management software? Why?')
+answer = ('	The question of, at what size of a project should you start using version management software, is quite subjective. The reason I say that being, some individuals choose to use version management software just for there own personal projects. This could be to stay organized while making a list of changes, to keep a timeline of your changes, or to have an easy way to access changes you’ve made in the past. On the other hand, massive projects that have 100s if not 1000s of people making multiple changes a day use version management software for all the same reasons. The size of the project may have an effect on what version management software an individual or group uses, based on preference and ease of access. But choosing to use, or not to use version management software boils down to opinion, rather than the size of the project. Is it more practical to use a software such as GitHub when working with 40 other individuals on a project, all of whom are making multiple changes a day? Surely, but there is nothing wrong with an individual using said software for something like, keeping track of and turning in classwork for college either! ')
+# Printing to screen
+print('My name is', name +'. ' 'The date is', date +'. ' 'Im answering the question;', question +'. ' )
+print(answer)
