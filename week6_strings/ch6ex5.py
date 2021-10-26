8853d9186a299d88e6c0fbb4b201872897674bbc
ortizcam <90529335+ortizcam@users.noreply.github.com>
2021-10-26 15:11:34 -0400

    Delete week6_strings directory

--- week6_strings/ch6ex5.py
+++
@@ -1,6 +0,0 @@
-str = 'X-DSPAM-Confidence:0.8475'  #string example
-start = str.find(':') #searching for semicolon
-out = str[start+1:] #Moving forward one position from semicolon
-out = float(out)
-kind = type(out)
-print(out)
